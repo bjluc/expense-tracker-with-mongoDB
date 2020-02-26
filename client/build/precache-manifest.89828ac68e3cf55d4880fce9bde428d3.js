3a335698b57711bd33849e3ab096f59f372ba95b
Luc Audemard <bjluc@bjluc.co.uk>
2020-02-26 20:11:37 +0000

    Deploy Commit

---
+++ client/build/precache-manifest.89828ac68e3cf55d4880fce9bde428d3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "baadd4b57f0ccfd226e1c9b28b69f493",
+    "url": "/index.html"
+  },
+  {
+    "revision": "db9bf8f9680ea13fc31d",
+    "url": "/static/css/main.73b46aa8.chunk.css"
+  },
+  {
+    "revision": "ac7a5c3e32cd02e84a22",
+    "url": "/static/js/2.c166ff5e.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.c166ff5e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "db9bf8f9680ea13fc31d",
+    "url": "/static/js/main.fec388d9.chunk.js"
+  },
+  {
+    "revision": "00fc4afc96365658fc09",
+    "url": "/static/js/runtime-main.eda899bb.js"
+  }
+]);
